67ddf5777b9ef6050ea8a9c4e8c59d2dcd1d9498
moshito <mosho@me.com>
2025-10-20 22:55:02 +0200

    Klar uppgift

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,80 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [nyNum1, setValue1] = useState("")
-  const [nyNum2, setValue2] = useState("")
-  const [funOp, setFunOp] = useState("")
-  const [showResults, setshowResults] = useState("")
+console.log("Powered by MoS\n==========")
 
-  const input1 = (e) => {
-    setValue1(e.target.value)
-  }
-  const input2 = (e) => {
-    setValue2(e.target.value)
+function App() {
+  const [value1, setValue1] = useState("")
+  const [value2, setValue2] = useState("")
+  const [funOp, setFunOp] = useState("")
+  const [showResults, setShowResults] = useState("")
+
+/*   //Denna ska visa display
+  const [display, showOnDisplay] = useState("") */
+
+  const input1 = (e) => setValue1(e.target.value)
+  const input2 = (e) => setValue2(e.target.value)
+
+  // Funktion som gör själva beräkningen
+  const calc = (value1, value2, funOp) => {
+    const n1 = Number(value1)
+    const n2 = Number(value2)
+
+    switch (funOp) {
+      case '+':
+        return n1 + n2
+      case '-':
+        return n1 - n2
+      case '*':
+        return n1 * n2
+      case '/':
+        return n2 !== 0 ? n1 / n2 : "Fel: /0"
+      default:
+        return "Ingen operator"
+    }
   }
 
   const btnOp = (e) => {
     setFunOp(e.target.textContent)
-    setshowResults(nyNum1+funOp+nyNum2)
   }
+
+  const evaluate = () => {
+    const result = calc(value1, value2, funOp)
+    setShowResults(result)
+    console.log("Du borde se resultat!")
+  }
+
   const clearBtn = () => {
-    setshowResults("")
+    setValue1("")
+    setValue2("")
+    setFunOp("")
+    setShowResults("")
     console.log("Cleared showResults")
   }
 
   return (
     <>
-      <div id="main">
+      <div id="calcH">
         <div>
-          <input type='number' onChange={input1} />
+          <input type='number' value={value1} onChange={input1} />
+        </div>
+
+        <div>
+          <button onClick={btnOp}>+</button>
+          <button onClick={btnOp}>-</button>
+          <button onClick={btnOp}>*</button>
+          <button onClick={btnOp}>/</button>
         </div>
         <div>
-          <input type='number' onChange={input2} /> 
+          <input type='number' value={value2} onChange={input2} /> 
+        </div>
+
+        <div>
+          <button onClick={clearBtn}>C</button>
+          <button onClick={evaluate}>=</button>
         </div>
         <div>
          <h1>Här: {showResults}</h1>
-        </div>
-        
-        <div>
-          <button onClick={btnOp} >+</button>
-          <button onClick={btnOp} >-</button>
-          <button onClick={btnOp} >*</button>
-          <button onClick={btnOp} >/</button>
-        </div>
-
-        <div >
-          <button onClick={clearBtn} >C</button>
         </div>
       </div>
     </>
@@ -52,4 +82,3 @@ function App() {
 }
 
 export default App
-
